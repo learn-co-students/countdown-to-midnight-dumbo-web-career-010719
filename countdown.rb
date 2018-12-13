dabb453cc53e74fe29dc3543f89f9655d91f9916
avid-modal-4107 <norbertl@buffalo.edu>
2018-12-13 06:52:03 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  while number != 0
+
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(arg)
+  while arg >= 0
+    puts "#{arg} SECOND(S)!"
+    arg -= 1
+    sleep 1
+  end
+  "HAPPY NEW YEAR!"
 end

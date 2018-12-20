32e3b015e8f9c1a254946a988267456a77fdbf10
zaheer <m.abbaszaheer@gmail.com>
2018-12-20 14:10:00 -0500

    countdown to midnight

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
 
-def countdown
+def countdown_with_sleep (number_of_second_you_want_to_pause)
+	sleep (number_of_second_you_want_to_pause)
 end
+
+def countdown (time_in_seconds)
+number=time_in_seconds
+while number !=0
+    puts "#{number} SECOND(S)!"		
+  		number -=1
+  		countdown_with_sleep (1)
+	end
+happy_new_year="HAPPY NEW YEAR!"
+end
+
